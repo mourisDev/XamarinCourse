29715f97c6268c8e5549e03253ba23b2fbbe1538
mourisDev <mouris13.dev@gmail.com>
2018-09-27 23:36:07 -0300

    App#01 - Complete

---
+++ xamaCourse01/xamaCourse01/xamaCourse01/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using xamaCourse01.Service.Model;
+using xamaCourse01.Service;
+
+namespace xamaCourse01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBuscar.Clicked += buscarCep;
+
+        }
+
+        private void buscarCep(object sender, EventArgs args)
+        {
+            string cep = txtCep.Text.Trim();
+            if (isValidCep(cep))
+            {
+                try
+                {
+                    address adr = viaCEPService.GetAddrCEP(cep);
+                    if (adr != null)
+                    {
+                        lblResult.Text = string.Format("Endereço: {0}, {1} {2}", adr.localidade, adr.uf, adr.logradouro);
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "CEP não encontrado, " + txtCep.Text.Trim(), "OK");
+                    }
+                }catch(Exception e)
+                {
+                    DisplayAlert("Erro", e.Message, "OK");
+                    
+                }
+            }
+        }
+
+        private bool isValidCep(string cep)
+        {
+            if (cep.Length != 8)
+            {
+                DisplayAlert("Erro", "Cep inválido! O CEP deve conter 8 caracteres.", "OK");
+                return false;
+            }
+
+            int novoCep = 0;
+            if(!int.TryParse(cep, out novoCep))
+            {
+                DisplayAlert("Erro", "Cep inválido! O CEP conter apenas números.", "OK");
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ xamaCourse01/xamaCourse01/xamaCourse01/Service/viaCEPService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using xamaCourse01.Service.Model;
+using Newtonsoft.Json;
+
+namespace xamaCourse01.Service
+{
+    public class viaCEPService
+    {
+        public static string uURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static address GetAddrCEP(string cep)
+        {
+            string newuURL = string.Format(uURL, cep);
+
+            WebClient wc = new WebClient();
+            string content = wc.DownloadString(newuURL);
+
+            address addrNew = JsonConvert.DeserializeObject<address>(content);
+
+            if (addrNew.cep == null) return null;
+            return addrNew;
+        }
+
+    }
+}
